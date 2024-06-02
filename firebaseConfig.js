ac22a1a073a0b02715e0a2ddba3d1e89618ea53d
Roma Joshi <roma@Romas-MacBook-Air.local>
2024-06-02 13:53:19 +0800

    halfway through firebase, about to change package.json.main

---
+++ firebaseConfig.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCdlayzke5r2CNoLZpGWRonIkurQPg6hZk",
+    authDomain: "unihealth-3e9df.firebaseapp.com",
+    databaseURL: "https://unihealth-3e9df-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "unihealth-3e9df",
+    storageBucket: "unihealth-3e9df.appspot.com",
+    messagingSenderId: "514012527971",
+    appId: "1:514012527971:web:04f79a7e749719b0aee756",
+    measurementId: "G-6V0PESJYVV"
+  };
+  
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
